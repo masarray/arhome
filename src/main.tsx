e68e0eb347028f03e9fbe303de4ab2bcfc52b5a5
masarray <ari.sulistiono@gmail.com>
2026-06-21 06:00:27 +0700

    Switch runtime font loading to Quicksand stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,3 @@
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/outfit/500.css";
-import "@fontsource/outfit/600.css";
-import "@fontsource/outfit/700.css";
-import "@fontsource/outfit/800.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
